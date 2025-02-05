798516e56c9938b540580a87d92027b11ae89471
dirui <dirui1@yeah.net>
2025-02-05 13:22:39 +0800

    no message

--- files/api/config/db.php
+++ files/api/config/db.php
@@ -2,7 +2,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=' . getenv('MYSQL_HOST') . ';dbname=' . getenv('MYSQL_DB'),
-    'username' => getenv('MYSQL_NAME'),
+    'username' => getenv('MYSQL_USERNAME'),
     'password' => getenv('MYSQL_PASSWORD'),
     'charset' => 'utf8',
 ];
