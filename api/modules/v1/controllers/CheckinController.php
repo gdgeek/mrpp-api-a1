4f7517d8d94c5323af0ea1b4721fc30007f45b86
gdgeek <dirui1@yeah.net>
2025-09-06 21:03:25 +0800

    no message

--- api/modules/v1/controllers/CheckinController.php
+++ api/modules/v1/controllers/CheckinController.php
@@ -22,30 +22,25 @@ class CheckinController extends \yii\rest\Controller
     }
     public function actionList()
     {
-        
+
+
         $searchModel = new SnapshotSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
         // $dataProvider->query->andWhere(['author_id' => Yii::$app->user->id]);
 
 
-        $tags = Yii::$app->request->get('tags');
 
-        // 如果tags参数存在，将其转换为数字数组
-        if ($tags) {
-            $tagsArray = array_map('intval', explode(',', $tags));
-            if (isset($tagsArray) && !empty($tagsArray)) {
-                // 使用 ActiveQuery 实例，无需强制转换
-                $query = $dataProvider->query;
-                //链接tag表进行过滤。tag表的key为checkin,通过verse_tags链接
-                $query->innerJoin('verse_tags', 'verse_tags.verse_id  = snapshot.verse_id')
-                    ->innerJoin('tags', 'tags.id = verse_tags.tags_id')
-                    ->andWhere(['tags.key' => 'checkin'])->groupBy('verse.id');
-                
-               /* $query->innerJoin('verse_tags', 'verse_tags.verse_id  = snapshot.verse_id')
-                    ->andWhere(['in', 'verse_tags.tags_id', $tagsArray])
-                    ->groupBy('verse.id'); // 避免重复结果
-            }
-        }
+        $query = $dataProvider->query;
+        //链接tag表进行过滤。tag表的key为checkin,通过verse_tags链接
+        $query->innerJoin('verse_tags', 'verse_tags.verse_id  = snapshot.verse_id')
+            ->innerJoin('tags', 'tags.id = verse_tags.tags_id')
+            ->andWhere(['tags.key' => 'checkin'])->groupBy(['snapshot.id']);
+
+        /* $query->innerJoin('verse_tags', 'verse_tags.verse_id  = snapshot.verse_id')
+             ->andWhere(['in', 'verse_tags.tags_id', $tagsArray])
+             ->groupBy('verse.id'); // 避免重复结果*/
+
+
 
         return $dataProvider;
     }
