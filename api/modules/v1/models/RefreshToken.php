bdfcc6f73b7f31ac7f5d0c57bbe2b49baef6cade
gdgeek <dirui1@yeah.net>
2025-10-10 17:45:22 +0800

    no message

--- api/modules/v1/models/RefreshToken.php
+++ api/modules/v1/models/RefreshToken.php
@@ -1,78 +1,13 @@
 <?php
-
 namespace app\modules\v1\models;
-
-use Yii;
-use yii\behaviors\TimestampBehavior;
-use yii\db\Expression;
-/**
- * This is the model class for table "refresh_token".
- *
- * @property int $id
- * @property int $user_id
- * @property string $key
- * @property string|null $created_at
- *
- * @property User $user
- */
-class RefreshToken extends \yii\db\ActiveRecord
+class RefreshToken extends \yii\redis\ActiveRecord
 {
-
-        public function behaviors()
-    {
-        return [
-            [
-                'class' => TimestampBehavior::class,
-                'attributes' => [
-                    \yii\db\ActiveRecord::EVENT_BEFORE_INSERT => ['created_at'],
-                ],
-                'value' => new Expression('NOW()'),
-            ],
-        ];
-    }
-
     /**
-     * {@inheritdoc}
+     * @return array 此记录的属性列表
      */
-    public static function tableName()
+    public function attributes()
     {
-        return 'refresh_token';
+        return ['id', 'user_id', 'key'];
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-            [['user_id', 'key'], 'required'],
-            [['user_id'], 'integer'],
-            [['created_at'], 'safe'],
-            [['key'], 'string', 'max' => 255],
-            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function attributeLabels()
-    {
-        return [
-            'id' => Yii::t('app', 'ID'),
-            'user_id' => Yii::t('app', 'User ID'),
-            'key' => Yii::t('app', 'Key'),
-            'created_at' => Yii::t('app', 'Created At'),
-        ];
-    }
-
-    /**
-     * Gets query for [[User]].
-     *
-     * @return \yii\db\ActiveQuery
-     */
-    public function getUser()
-    {
-        return $this->hasOne(User::className(), ['id' => 'user_id']);
-    }
 }
